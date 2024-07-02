d2167ad45a3a489249f7413fb695d785f755f8f4
Matt Norton <matt@carrotmanmatt.com>
2024-07-02 01:59:07 +0100

    Add site meta tags for social media embeds
    Closes #4

--- .build_and_deploy/build.py
+++ .build_and_deploy/build.py
@@ -50,8 +50,10 @@ def build_single_page(*, html_file_path: Path) -> str:
         do_not_minify_doctype=True,
         keep_html_and_head_opening_tags=True,
         ensure_spec_compliant_unquoted_attribute_values=True,
+        keep_spaces_between_attributes=True,
         minify_js=True,
         minify_css=True,
+        keep_comments=False,
     ).replace("<!doctype html>", "<!DOCTYPE HTML>")
 
     if copyright_comment_match:
