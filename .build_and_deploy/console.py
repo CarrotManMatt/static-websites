3381e9b43d6f2afab99eff0acfc3e505b2f86726
Matt Norton <matt@carrotmanmatt.com>
2024-07-04 17:30:05 +0100

    Fix argument names again

--- .build_and_deploy/console.py
+++ .build_and_deploy/console.py
@@ -47,7 +47,7 @@ def _set_up_arg_parser(given_arguments: Sequence[str] | None = None) -> Argument
         usage=(
             "[-h/--help]\n       "
             "[-d/--remote-directory REMOTE_DIRECTORY]\n       "
-            "[-u/--remote-username REMOTE_USERNAME]\n       "
+            "[-u/--remote-user REMOTE_USER]\n       "
             "[-D/--dry-run]\n       "
             "[-v/--verbose | -q/--quiet]"
         ),
@@ -66,10 +66,10 @@ def _set_up_arg_parser(given_arguments: Sequence[str] | None = None) -> Argument
 
     arg_parser.add_argument(
         "-u",
-        "--remote-username",
+        "--remote-user",
         type=Username,
         help=(
-            "The username on the webserver to deploy static websites to. "
+            "The username of the user on the webserver to deploy static websites to. "
             "(Defaults to the root user if not specified.)"
         ),
     )
