576b5497f926861a78fd3abf43c181518af889a5
Matt Norton <matt@carrotmanmatt.com>
2026-05-04 19:18:45 +0100

    Add character guide and script to olympic show site

--- sites/olympic_show_uk.py
+++ sites/olympic_show_uk.py
@@ -91,14 +91,114 @@ PAGES_MAP: Final[Mapping[PurePosixPath, h.HTMLElement]] = {
                             "cz0qqdo c1p7rcrf c1tgzf95"
                         )
                     )[_show_dates],
-                    h.p(
+                    h.ul(
                         class_=(
-                            "w-element ckr079c ca4u039 ckupqx5 c1bleucc c9895ho c7uxvde "
-                            "cz0qqdo c1p7rcrf c1tgzf95"
+                            "w-element c1n5tuq8 c1fjw25m cnzpwoq crpyobo c15vmzu4 c1idw5j2 "
+                            "c1rwodom c12n57os cli8a60 cgtv1h1 c168ej5r c1xruc57"
                         )
                     )[
-                        "Casting emails have now been sent, "
-                        "please check your inbox for more information!"
+                        h.li(
+                            class_=(
+                                "w-element cu52st3 caaqcxl c18y1yfe c1uom0t8 c1b3o6vr cc1a8ib "
+                                "cryncd3 cd3i0tr c21ee66 c1l6ehxo cbvcuo2 c69pgid c79tq8k "
+                                "c1gh997e"
+                            )
+                        )[
+                            h.a(
+                                href="/script",
+                                class_=(
+                                    "w-element c19y3cjm c1lglyie c14wber8 cwfy5au cusinh7 "
+                                    "c1hya6s0 cq6mi0e c15vmzu4 c1idw5j2 crpyobo c1u759pa "
+                                    "c1gm7vb"
+                                ),
+                            )[
+                                h.span(class_="w-element ca4u039 cj9275l")["Script"],
+                                h.svg(
+                                    xmlns="http://www.w3.org/2000/svg",
+                                    viewbox="0 0 640 640",
+                                    class_="w-element czzn447",
+                                )[
+                                    h.path(
+                                        fill="rgb(255, 255, 255)",
+                                        d=(
+                                            "M32 176C32 134.5 63.6 100.4 104 96.4L104 96L384 "
+                                            "96C437 96 480 139 480 192L480 368L304 368C264.2 "
+                                            "368 232 400.2 232 440L232 500C232 524.3 212.3 "
+                                            "544 188 544C163.7 544 144 524.3 144 500L144 "
+                                            "272L80 272C53.5 272 32 250.5 32 224L32 "
+                                            "176zM268.8 544C275.9 530.9 280 515.9 280 500L280 "
+                                            "440C280 426.7 290.7 416 304 416L552 416C565.3 "
+                                            "416 576 426.7 576 440L576 464C576 508.2 540.2 "
+                                            "544 496 544L268.8 544zM112 144C94.3 144 80 158.3 "
+                                            "80 176L80 224L144 224L144 176C144 158.3 129.7 "
+                                            "144 112 144z"
+                                        ),
+                                        class_="w-element",
+                                    )
+                                ],
+                            ]
+                        ],
+                        h.li(
+                            class_=(
+                                "w-element cu52st3 caaqcxl c18y1yfe c1uom0t8 c1b3o6vr cc1a8ib "
+                                "cryncd3 cd3i0tr c21ee66 c1l6ehxo cbvcuo2 c69pgid c79tq8k "
+                                "c1gh997e"
+                            )
+                        )[
+                            h.a(
+                                href="/character-guide",
+                                class_=(
+                                    "w-element c19y3cjm c1lglyie c14wber8 c1hya6s0 cvp7t4m "
+                                    "cwfy5au cusinh7 c15vmzu4 c1idw5j2 crpyobo c1u759pa "
+                                    "c1gm7vb"
+                                ),
+                            )[
+                                h.span(class_="w-element ca4u039 cryncd3 cj9275l")[
+                                    "Character Guide"
+                                ],
+                                h.svg(
+                                    xmlns="http://www.w3.org/2000/svg",
+                                    viewbox="0 0 640 640",
+                                    class_="w-element czzn447",
+                                )[
+                                    h.path(
+                                        fill="rgb(255, 255, 255)",
+                                        d=(
+                                            "M27 182L55.5 343.7C69.5 423.2 131.8 485.5 211.3 "
+                                            "499.5L224 501.7C207.5 473.1 196.9 441 193.4 "
+                                            "407.2L169.3 411.5C159.6 413.2 150.5 405.7 152.4 "
+                                            "396C157.2 371.3 171.5 349.4 192.1 335.1L192.1 "
+                                            "260.5C190.7 261.3 189.1 261.8 187.4 262.1L124.4 "
+                                            "273.2C115.7 274.7 107.1 268.8 108.5 260.1C111.6 "
+                                            "240.5 126.9 224.1 147.6 220.4C164.8 217.4 181.5 "
+                                            "223.9 192.2 236.2L192.2 213.5C192.2 191 199.1 "
+                                            "161.1 224.5 140.1C250.5 118.6 292.2 96.2 349.4 "
+                                            "85.9C318.9 69.6 263.1 53.9 185.6 67.5C105.3 81.7 "
+                                            "57.6 117.6 35.5 143.6C26.5 154.1 24.7 168.5 27.1 "
+                                            "182.1zM240 202.7L240 377.5C240 458.2 290.5 530.4 "
+                                            "366.4 557.9L394.1 568C408.2 573.1 423.7 573.1 "
+                                            "437.8 568L465.6 558C541.5 530.4 592 458.3 592 "
+                                            "377.5L592 202.7C592 195.8 589.9 188.9 585 "
+                                            "184.1C562.4 161.6 506.8 128.1 416 128.1C325.2 "
+                                            "128.1 269.6 161.7 247 184.1C242.1 189 240 195.8 "
+                                            "240 202.7zM306.1 389.8C304.7 382.8 313.1 378.8 "
+                                            "318.8 383.2C345.7 403.8 379.4 416.1 416 "
+                                            "416.1C452.6 416.1 486.2 403.8 513.2 383.2C518.9 "
+                                            "378.8 527.3 382.8 525.9 389.8C515.8 441.2 470.4 "
+                                            "480.1 416 480.1C361.6 480.1 316.2 441.3 306.1 "
+                                            "389.8zM306.6 288.3C313.2 269.5 331 256 352 "
+                                            "256C373 256 390.9 269.5 397.4 288.3C400.3 296.7 "
+                                            "392.9 304 384 304L320 304C311.2 304 303.7 296.6 "
+                                            "306.6 288.3zM512 304L448 304C439.2 304 431.7 "
+                                            "296.6 434.6 288.3C441.1 269.5 459 256 480 "
+                                            "256C501 256 518.9 269.5 525.4 288.3C528.3 296.7 "
+                                            "520.9 304 512 304z"
+                                        ),
+                                        class_="w-element",
+                                    )
+                                ],
+                            ]
+                        ],
                     ],
                 ],
                 h.div(
@@ -221,14 +321,14 @@ PAGES_MAP: Final[Mapping[PurePosixPath, h.HTMLElement]] = {
             ],
             h.script(id="vike_pageContext", type="application/json")[
                 '{"_urlRewrite":null,"pageId":"/pages/index","routeParams":{},"data":{'
-                '"url":"https://url/","system":{'
-                '"params":{},"search":{},"origin":"https://url","pathname":"/"},'
-                f'"resources":{{}},"pageMeta":{{"title":"{_site_title.strip('\n\r\t ",')}",'
-                f'"description":"{_site_description.strip('\n\r\t ",')}",'
-                '"excludePageFromSearch":"!undefined","language":"en-GB",'
-                '"socialImageAssetName":"!undefined",'
-                f'"socialImageUrl":"{_site_url}{_logo_location}",'
-                '"status":"!undefined","redirect":"!undefined","custom":[]}}}'
+                f'"url":"https://url/","system":{{"resources":{{}},"pageMeta":{{"title":"{
+                    _site_title.strip('\n\r\t ",')
+                }","description":"{
+                    _site_description.strip('\n\r\t ",')
+                }","excludePageFromSearch":"!undefined","language":"en-GB",'
+                f'"socialImageAssetName":"!undefined","socialImageUrl":"{_site_url}{
+                    _logo_location
+                }","status":"!undefined","redirect":"!undefined","custom":[]}}}}}}'
             ],
             h.script(id="vike_globalContext", type="application/json")[" {}"],
             h.script(src="/static/js/entry-server-routing.js", type="module", async_=True),
